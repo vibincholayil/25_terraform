567242d30155c6ec538da36aff8c652f24d58aaa
vibincholayil <vibinnath.c@gmail.com>
2025-05-27 17:10:50 +0100

    update

---
+++ terraform-multi-env-infra0/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = "${terraform.workspace}-ec2-instance"
+    Environment = terraform.workspace
+  }
+}
